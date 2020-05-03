045673c779ec99666958aa5fc5461294d667e9fe
Bakhtiyar Habib <bakhtiyar.habib19@gmail.com>
2020-05-04 02:08:13 +0600

    added categories and brands apps to project

---
+++ src/brands/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Brands(models.Model):
+	title = models.CharField(max_length=120)
+	description = models.TextField(blank=True, null=True)
+	photo = models.ImageField(default = "default.jpg", blank=True)
---
+++ src/categories/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Categories(models.Model):
+	title = models.CharField(max_length=120)
+	description = models.TextField(blank=True, null=True)
+	photo = models.ImageField(default = "default.jpg", blank=True)
+	subcategories = models.CharField(max_length=120, null = True)
